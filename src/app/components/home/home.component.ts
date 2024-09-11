928fb38ad093f4979b04b4ff5040709f312aefc4
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2024-09-11 11:15:40 +0530

    second commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,31 +1,53 @@
-import { Component, ElementRef, model, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, model, ViewChild ,ChangeDetectorRef } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatDatepickerModule,MatCardModule],
+  imports: [MatDatepickerModule,MatCardModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  selected = model<Date | null>(null);
+export class HomeComponent implements AfterViewInit{
   @ViewChild('scrollable', { static: false }) scrollable!: ElementRef;
+  isAtStart = true;
+  isAtEnd = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.updateButtonVisibility();
+    this.scrollable.nativeElement.addEventListener('scroll', () => {
+      this.updateButtonVisibility();
+      this.cdr.detectChanges(); 
+    });
+  }
 
   scrollLeft() {
     this.scrollable.nativeElement.scrollBy({
-      left: -200, // Adjust scroll distance as needed
+      left: -200, 
       behavior: 'smooth'
     });
+    this.updateButtonVisibility();
   }
 
   scrollRight() {
     this.scrollable.nativeElement.scrollBy({
-      left: 200, // Adjust scroll distance as needed
+      left: 200, 
       behavior: 'smooth'
     });
+    this.updateButtonVisibility();
+  }
+
+  updateButtonVisibility() {
+    const container = this.scrollable.nativeElement;
+    const maxScrollLeft = container.scrollWidth - container.clientWidth;
+    this.isAtStart = container.scrollLeft === 0;
+    this.isAtEnd = container.scrollLeft >= maxScrollLeft;
+    this.cdr.detectChanges();
   }
 }
