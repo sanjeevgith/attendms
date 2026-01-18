7101210511cb575bcb22923a57366b126650949c
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2026-01-18 12:22:05 +0530

    all module files added

---
+++ src/app/modules/helpdesk/helpdesk.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-helpdesk',
+  standalone: true,
+  imports: [],
+  templateUrl: './helpdesk.component.html',
+  styleUrl: './helpdesk.component.scss'
+})
+export class HelpdeskComponent {
+
+}
